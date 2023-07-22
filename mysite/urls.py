2b7ded626730776ff653298bb5af60eeef880293
z-scientist-ai <82810150+z-scientist-ai@users.noreply.github.com>
2023-07-22 15:34:16 +0330

    add url

--- mysite/urls.py
+++ mysite/urls.py
@@ -33,6 +33,7 @@ sitemaps = {
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls', namespace='blog')),
+    path('', include('blog.urls', namespace='blog')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
      name='django.contrib.sitemaps.views.sitemap'),
     path('api/v1/',include('blog.urls')),
